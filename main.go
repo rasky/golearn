33440fceb29eef33215828900d2516324db7a8af
Giovanni Bajo <rasky@develer.com>
2015-05-27 14:34:24 +0200

    Initial versione

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+// Click on this link to view the spreadsheet
+// https://docs.google.com/a/develer.com/spreadsheets/d/1R7xpW4LTGj__B-W8qFKTxYst0LTJBR_AgNcyedbhvwI/edit?usp=sharing
+
+const CSV_URL = "https://docs.google.com/spreadsheets/d/1R7xpW4LTGj__B-W8qFKTxYst0LTJBR_AgNcyedbhvwI/export?format=csv"
+
+func main() {
+
+	// STEP 1: implement CSV download
+
+}
