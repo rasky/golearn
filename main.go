bcabafc6b59e43a6dba41c1dfb579bfe64162655
Giovanni Bajo <rasky@develer.com>
2015-05-27 14:40:01 +0200

    Add data structur

--- main.go
+++ main.go
@@ -5,12 +5,21 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 )
 
 // Click on this link to view the spreadsheet
 // https://docs.google.com/a/develer.com/spreadsheets/d/1R7xpW4LTGj__B-W8qFKTxYst0LTJBR_AgNcyedbhvwI/edit?usp=sharing
 
 const CSV_URL = "https://docs.google.com/spreadsheets/d/1R7xpW4LTGj__B-W8qFKTxYst0LTJBR_AgNcyedbhvwI/export?format=csv"
+
+type Person struct {
+	Nickname string
+	Name     string
+	Surname  string
+	Birthday time.Time
+	Tags     []string
+}
 
 func main() {
 
