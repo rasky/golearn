1496a99b0ee2f714f6c96bae7b84dd184d394313
Giovanni Bajo <rasky@develer.com>
2015-05-27 14:36:24 +0200

    Implemented download and print to stdout

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
 // Click on this link to view the spreadsheet
 // https://docs.google.com/a/develer.com/spreadsheets/d/1R7xpW4LTGj__B-W8qFKTxYst0LTJBR_AgNcyedbhvwI/edit?usp=sharing
 
@@ -8,5 +15,14 @@ const CSV_URL = "https://docs.google.com/spreadsheets/d/1R7xpW4LTGj__B-W8qFKTxYs
 func main() {
 
 	// STEP 1: implement CSV download
-
+	res, err := http.Get(CSV_URL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	data, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s", data)
 }
